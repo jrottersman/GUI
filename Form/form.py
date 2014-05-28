adb76da5b0de26df6ba0dbd2e9570ba174badf99
jrottersman <rottersmanjake@gmail.com>
2014-05-28 10:59:07 -0400

    Working

--- Form/form.py
+++ Form/form.py
@@ -7,10 +7,12 @@ class simpleapp_tk(Tkinter.Tk):
 		
     def initialize(self):
         self.grid()
-		
-        self.entry = Tkinter.Entry(self)
+        
+        self.entryVariable = Tkinter.StringVar()
+        self.entry = Tkinter.Entry(self, textvariable=self.entryVariable)
         self.entry.grid(column=0, row=0, sticky='EW')
         self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u"Enter text here.")
 		
         button = Tkinter.Button(self, text=u"Click me!",
                                command=self.OnButtonClick)
@@ -20,15 +22,16 @@ class simpleapp_tk(Tkinter.Tk):
         label = Tkinter.Label(self,textvariable=self.labelVariable,
                               anchor='w', fg="white",bg="blue")
         label.grid(column=0,row=1,columnspan=2, sticky='EW')
+        self.labelVariable.set(u"Hello!")
 		
         self.grid_columnconfigure(0, weight=1)
         self.resizable(True,False)
 
 		
     def OnButtonClick(self):
-        self.labelVariable.set("You clicked the button!")
+        self.labelVariable.set(self.entryVariable.get() + " (You clicked the button!)")
     def OnPressEnter(self,event):
-       self.labelVariable.set("You pressed enter!")
+       self.labelVariable.set(self.entryVariable.get() + " (You pressed enter!)")
 		
 if __name__ == "__main__":
     app = simpleapp_tk(None)
